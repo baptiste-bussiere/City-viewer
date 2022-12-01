c169d7ca303cb0c00018448f853134f22c6effd2
Baptiste-bussiere <contact@baptiste-bussiere.fr>
2022-12-01 12:25:46 +0100

    clean project

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 https://sketchfab.com/3d-models/lowpoly-city-605a0998c55c47fc932821611c6a09f3
+
+
--- src/script.js
+++ src/script.js
@@ -8,234 +8,54 @@ import * as dat from 'lil-gui'
 
 /**  ANIMATION JAVASCRIPT HTML **/
 
-gsap.to(".translate", {
-    translateY: 0,
-    delay: 0.5,
-    duration: 2.5,
-    ease: "expo.out"
 
-})
+
+/**          DEBUG            **/
+// const gui = new dat.GUI()
+
+
+
+
+/**          CANVAS           **/
+const canvas = document.querySelector('canvas.webgl')
+
+
+
+/**          SCENE            **/
+const scene = new THREE.Scene()
+
+
+
+/**          MODELS           **/
+
+const gltfLoader = new GLTFLoader()
+
+let city;
+gltfLoader.load('/models/toile-3.glb', function(gltf) {
+
+    city = gltf.scene;
+    city.position.set(0, 0, 0)
+    scene.add(city)
+
+});
+
+
+/**          CURSOR            **/
 
 const cursor = {
     x: 0,
     y: 0
 }
 
-
-
-
-cur1.addEventListener('mouseover', (event) => {
-
-    gsap.to(point1, {
-        opacity: 1,
-    })
-})
-
-cur1.addEventListener('mouseout', (event) => {
-    gsap.to(point1, {
-        opacity: 0,
-    })
-})
-cur2.addEventListener('mouseover', (event) => {
-
-    gsap.to(point2, {
-        opacity: 1,
-    })
-})
-
-cur2.addEventListener('mouseout', (event) => {
-    gsap.to(point2, {
-        opacity: 0,
-    })
-})
-cur3.addEventListener('mouseover', (event) => {
-
-    gsap.to(point3, {
-        opacity: 1,
-    })
-})
-
-cur3.addEventListener('mouseout', (event) => {
-    gsap.to(point3, {
-        opacity: 0,
-    })
-})
-cur4.addEventListener('mouseover', (event) => {
-
-    gsap.to(point4, {
-        opacity: 1,
-    })
-})
-
-cur4.addEventListener('mouseout', (event) => {
-    gsap.to(point4, {
-        opacity: 0,
-    })
-})
-cur5.addEventListener('mouseover', (event) => {
-
-    gsap.to(point5, {
-        opacity: 1,
-    })
-})
-
-cur5.addEventListener('mouseout', (event) => {
-    gsap.to(point5, {
-        opacity: 0,
-    })
-})
-cur6.addEventListener('mouseover', (event) => {
-
-    gsap.to(point6, {
-        opacity: 1,
-    })
-})
-
-cur6.addEventListener('mouseout', (event) => {
-    gsap.to(point6, {
-        opacity: 0,
-    })
-})
-cur7.addEventListener('mouseover', (event) => {
-
-    gsap.to(point7, {
-        opacity: 1,
-    })
-})
-
-cur7.addEventListener('mouseout', (event) => {
-    gsap.to(point7, {
-        opacity: 0,
-    })
-})
-cur8.addEventListener('mouseover', (event) => {
-
-    gsap.to(point8, {
-        opacity: 1,
-    })
-})
-
-cur8.addEventListener('mouseout', (event) => {
-    gsap.to(point8, {
-        opacity: 0,
-    })
-})
-
-cur8.addEventListener('mouseover', (event) => {
-
-    gsap.to(point8, {
-        opacity: 1,
-    })
-})
-
-cur8.addEventListener('mouseout', (event) => {
-    gsap.to(point8, {
-        opacity: 0,
-    })
-})
-
-
-const loaderContainer = document.querySelector('.loader');
-window.addEventListener('load', () => {
-
-    gsap.to(loaderContainer, {
-        opacity: 0,
-        display: "none"
-    })
-
-});
 window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX - innerWidth
-    cursor.y = event.clientY
-    point1.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point2.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point3.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point4.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point5.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point6.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point7.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    point8.style.transform = `translate(${cursor.x}px, ${cursor.y}px)`
-    curseur.style.transform = `translate(${cursor.x +9 }px, ${cursor.y- 9}px)`
-
-
-
+    cursor.x = event.clientX / sizes.width - 0.5,
+        cursor.y = event.clientY / sizes.height - 0.5
 })
 
 
 
+/**          LIGHTS            **/
 
-/**
- * Base
- */
-// Debug
-// const gui = new dat.GUI()
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Models
- */
-
-
-const gltfLoader = new GLTFLoader()
-
-
-var t1;
-var t2;
-var t3;
-var t4;
-
-gltfLoader.load('/models/toile-3.glb', function(gltf) {
-
-    t1 = gltf.scene;
-    t1.position.set(0, 0, 0)
-    scene.add(t1);
-
-});
-
-gltfLoader.load('/models/toile-2.glb', function(gltf) {
-
-    t2 = gltf.scene;
-    t2.scale.set(0.0015, 0.0015, 0.0015)
-
-    t2.position.set(5, 5, 0)
-    scene.add(t2);
-
-});
-
-gltfLoader.load('/models/toile-1.glb', function(gltf) {
-
-    t3 = gltf.scene;
-    t3.position.set(10, 0, 0)
-    scene.add(t3);
-
-});
-gltfLoader.load('/models/toile-4.glb', function(gltf) {
-
-    t4 = gltf.scene;
-    t4.position.set(15, 0, 0)
-    t4.scale.set(0.06, 0.06, 0.06)
-    scene.add(t4);
-
-});
-
-
-
-
-
-
-/**
- * Floor
- */
-
-
-/**
- * Lights
- */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
 scene.add(ambientLight)
 
@@ -250,101 +70,26 @@ directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(-5, 5, 0)
 scene.add(directionalLight)
 
-/**
- * Sizes
- */
+
+
+/**           SIZES            **/
+
 const sizes = {
-    width: window.innerWidth / 2,
+    width: window.innerWidth,
     height: window.innerHeight
 }
 
 
+/**         CAMERA           **/
 
-let click = false;
-
-
-
-banner.addEventListener('click', (event) => {
-    sizes.width = window.innerWidth
-    gsap.to(sizes, {
-        width: sizes.width,
-    })
-    gsap.to(banner, {
-        display: "none",
-        opacity: 0
-    })
-    gsap.to(cross, {
-        display: "inherit",
-        opacity: 1,
-        delay: 1
-    })
-
-
-    click = true
-
-
-})
-
-cross.addEventListener('click', (event) => {
-
-    sizes.width = window.innerWidth / 2,
-        gsap.to(cross, {
-            display: "none",
-            opacity: 0,
-
-        })
-    gsap.to(banner, {
-        display: "inherit",
-        opacity: 1,
-        delay: 1
-    })
-    click = false
-
-
-
-
-})
-
-
-
-
-/**
- * Camera
- */
-// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 1
-
-
-
-left.addEventListener("click", () => {
-    let x = camera.position.x
-    gsap.to(camera.position, {
-        x: x - 5,
-        duration: 3,
-        ease: 'ease.out'
-    })
-});
-
-right.addEventListener("click", () => {
-    let x = camera.position.x
-
-    gsap.to(camera.position, {
-        x: x + 5,
-        duration: 3,
-        ease: 'power4.in.out'
-    })
-});
-
-
+camera.position.set(0, 0, 1)
 scene.add(camera)
 
 
-/**
- * Renderer
- */
+
+/**        RENDERER         **/
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
@@ -353,121 +98,53 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
 
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+/**        RESIZE         **/
 
-    if (t1) {
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-        gsap.to(t1.rotation, {
-            x: cursor.y / sizes.height - 0.5,
-            y: cursor.x / sizes.width / 2 + 0.5,
-
-        })
-        t1.position.y = Math.sin(elapsedTime) * 0.05
-
-
-    }
-    if (t2) {
-
-        gsap.to(t2.rotation, {
-            x: cursor.y / sizes.height - 0.5,
-            y: cursor.x / sizes.width / 2 + 0.5,
-
-        })
-        t2.position.y = Math.sin(elapsedTime) * 0.05
-
-
-    }
-    if (t3) {
-        gsap.to(t3.rotation, {
-            x: cursor.y / sizes.height - 0.5,
-            y: cursor.x / sizes.width / 2 + 0.5,
-        })
-        t3.position.y = Math.sin(elapsedTime) * 0.05
-    }
-    if (t4) {
-        gsap.to(t4.rotation, {
-            x: cursor.y / sizes.height - 0.5,
-            y: cursor.x / sizes.width / 2 + 3.5,
-        })
-        t4.position.y = Math.sin(elapsedTime) * 0.05
-    }
-
-
-    if (camera.position.x == 0) {
-        gsap.to(left, {
-            display: 'none',
-            opacity: 0
-        })
-    } else {
-        gsap.to(left, {
-            display: 'inherit',
-            opacity: 1
-        })
-    }
-    if (camera.position.x == 20) {
-        gsap.to(right, {
-            display: 'none',
-            opacity: 0
-        })
-    } else {
-        gsap.to(right, {
-            display: 'inherit',
-            opacity: 1
-        })
-    }
-
-    if (click) {
-
-
-        window.addEventListener('resize', () => {
-            // Update sizes
-            sizes.width = window.innerWidth
-            sizes.height = window.innerHeight
-
-            // Update camera
-            camera.aspect = sizes.width / sizes.height
-            camera.updateProjectionMatrix()
-
-            // Update renderer
-            renderer.setSize(sizes.width, sizes.height)
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        })
-    } else(
-        window.addEventListener('resize', () => {
-            // Update sizes
-            sizes.width = window.innerWidth / 2
-            sizes.height = window.innerHeight
-
-            // Update camera
-            camera.aspect = sizes.width / sizes.height
-            camera.updateProjectionMatrix()
-
-            // Update renderer
-            renderer.setSize(sizes.width, sizes.height)
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        })
-    )
-
-
-
-
-    // Update controls
-    // controls.update()
-    // Render
-    renderer.render(scene, camera)
+    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-        // Call tick again on the next frame
+})
+
+/**        ANIMATE          **/
+
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    if (city) {
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // controls.update()
+
+    renderer.render(scene, camera)
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     window.requestAnimationFrame(tick)
 }
 
